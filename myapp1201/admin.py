04c5321a83ccb7d6d54e5e433b94aba8ef9b1e8e
takeru1201 <ldn.asktmhmh@icloud.com>
2020-10-28 14:51:23 +0900

    create:管理画面の作成
    admin.pyで管理画面にDirector,Movie,Log
    を追加

--- myapp1201/admin.py
+++ myapp1201/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+# models.pyで作った表を再利用
+from myapp1201.models import Director, Movie, Log
+
+admin.site.register(Director)
+admin.site.register(Movie)
+admin.site.register(Log)
+
+
 
 
 # Register your models here.
