ffb6949f16af42c7870eb037c6983f0e586c45e9
takeru1201 <ldn.asktmhmh@icloud.com>
2020-10-31 20:06:22 +0900

    Create views.py
    views.pyを作成
    クラスベース汎用ビューを活用

--- myapp1201/views.py
+++ myapp1201/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# views.pyはrailsでいうコントローラー
+# 汎用ビューを使用する為のgeneric
+from django.views import generic
+from myapp1201.models import Movie, Director, Log
 
-# Create your views here.
+# ListViewが汎用ビュー
+class IndexView(generic.ListView):
+    template_name = 'myapp1201/index.html'
+    context_object_name = 'movie_list'
+    queryset = Movie.objects.all()
+
+class MovieDetailView(generic.DetailView):
+    model = Movie
+    template_name = 'myapp1201/detail.html'
+
