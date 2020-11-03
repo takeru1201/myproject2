510d9c4482057b430a9c6fd17d25515edcaf6220
takeru1201 <ldn.asktmhmh@icloud.com>
2020-11-03 20:50:45 +0900

    add:register_urls
    情報を登録する為のURLを指定

--- myapp1201/urls.py
+++ myapp1201/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     # views.pyに定義されているIndexViewに定義されているIndexView関数として使用
     path('', views.IndexView.as_view(), name='index'),
     path('movie/<int:pk>/', views.MovieDetailView.as_view(), name='movie_detail'),
+    # viewsの後はクラス名views.pyで定義する
+    path('register/director/', views.RegisterDirectorView.as_view(), name='movie_diteil'),
+    path('redister/movie/', views.RegisterMovieView.as_view(), name='registermovie'),
+    path('writing/log', Views.WritingLogView.as_view(), name='writinglog'),
 ]
