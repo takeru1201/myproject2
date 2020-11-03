c1fd9f27c31356b0f8ede0750e5231d9e901422e
takeru1201 <ldn.asktmhmh@icloud.com>
2020-11-03 21:08:32 +0900

    add:RegisterDirector
    監督を登録する為のview

--- myapp1201/urls.py
+++ myapp1201/urls.py
@@ -7,6 +7,7 @@ app_name = 'myapp1201'
 urlpatterns = [
     # views.pyに定義されているIndexViewに定義されているIndexView関数として使用
     path('', views.IndexView.as_view(), name='index'),
+    # views.pyではMovieDetailView関数として定義、でもうrlで使うときはmovie_detail
     path('movie/<int:pk>/', views.MovieDetailView.as_view(), name='movie_detail'),
     # viewsの後はクラス名views.pyで定義する
     path('register/director/', views.RegisterDirectorView.as_view(), name='movie_diteil'),
--- myapp1201/views.py
+++ myapp1201/views.py
@@ -13,3 +13,14 @@ class MovieDetailView(generic.DetailView):
     model = Movie
     template_name = 'myapp1201/detail.html'
 
+class RegisterDirectorView(generic.CreateView):
+  # 格納するデータベースの項目
+  model = Director
+  # 上の項目を何に使い方
+  form_class = DirectorForm
+  入力画面を表示するHTMLファイルURL
+  template_name = 'myapp1201/register.html'
+  # データの入力と格納がうまくできた後の行先
+  def get_success_url(self):
+    # 行先はmyapp1201/register/movie
+    return reverse('myapp1201:registermovie')
